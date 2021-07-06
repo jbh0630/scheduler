2b26e64d8c59b69d43c8b3cff93cfd111795630b
jbh0630 <jayjo0630@gmail.com>
2021-07-06 00:58:45 -0700

    Styling the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,11 +9,21 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   });
-  
+
+  const formatSpot = (spot) => {
+    if (spot === 0) {
+      return 'no spots remaining';
+    } else if (spot === 1) {
+      return `${spot} spot remaining`;
+    } else {
+      return `${spot} spots remaining`;
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpot(props.spots)}</h3>
     </li>
   );
 }
