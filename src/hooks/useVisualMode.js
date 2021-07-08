af7caa5501f40b064ed7c189311e4abccfb51358
jbh0630 <jayjo0630@gmail.com>
2021-07-08 16:19:14 -0700

    Building a Hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace === true) {
+      setMode(mode);
+    } else {
+      setMode(mode);
+      setHistory([...history, mode]);
+    }
+    
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      history.pop();
+    } else {
+      setMode(initial);
+    }
+  }
+
+  return { mode, transition, back };
+}
