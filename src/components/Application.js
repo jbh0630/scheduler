569f39b02f711ccd0890248b00431f0e0a30711e
jbh0630 <jayjo0630@gmail.com>
2021-07-08 20:05:03 -0700

    Using Custom Hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+
 import "components/Application.scss";
 
 
@@ -12,7 +13,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   
   const appointments = getAppointmentsForDay(state, state.day);
@@ -21,9 +23,9 @@ export default function Application(props) {
   
   useEffect(() => {
     Promise.all([
-    axios.get("http://localhost:8001/api/days"),
-    axios.get("http://localhost:8001/api/appointments"),
-    axios.get("http://localhost:8001/api/interviewers"),
+    axios.get("/api/days"),
+    axios.get("/api/appointments"),
+    axios.get("/api/interviewers"),
   ]).then((all) => {
     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
   });
@@ -37,6 +39,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={state.interviewers}
       />
     );
   });
